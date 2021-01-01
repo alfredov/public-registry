f295f0fbe5c462fd88ba1bfe71bfe366b3d185bd
Alfredo Villagomez <alfredo.villagomez.m@gmail.com>
2021-01-01 15:56:44 -0400

    gitpkg

---
+++ dist/0.react-app-template.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{36:function(e,n,t){"use strict";t.r(n);var a=t(0),l=t.n(a);n.default=l.a.memo(function(){return l.a.createElement("div",null,l.a.createElement("h1",null,"Page not found"),l.a.createElement("p",null,"404"))})}}]);
